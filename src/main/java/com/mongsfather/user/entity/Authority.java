5934c1c56de4cc7c1edefab842526832cd7c7ac3
a02224 <a02224@hanatour.com>
2022-05-18 16:05:56 +0900

    누락파일 추가

---
+++ src/main/java/com/mongsfather/user/entity/Authority.java
@@ -0,0 +1,23 @@
+package com.mongsfather.user.entity;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+   @Id
+   @Column(name = "authority_name", length = 50)
+   private String authorityName;
+}
